94f18d15c75dac6572ccfc0fa09cc96ddc34b288
DARKDRAGON532 <69623818+DARKDRAGON532@users.noreply.github.com>
2020-10-15 21:41:26 +0530

    v2

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 
-if (typeof(Storage) !== "undefined") {
-    if (!localStorage.count) {
-        localStorage.setItem("count", "0");
-    } 
-} 
-let number = 0;
 const numberHTML = document.getElementById("number")
+
+if (typeof(Storage) !== "undefined") {
+    if (localStorage.count) {
+        numberHTML.innerHTML = localStorage.count
+      } else {
+        localStorage.setItem("count" , number)
+      }
+} 
+let number = Number(localStorage.count);
 
 function Increase(){
     number++;
-    localStorage.count = String(number);
+    localStorage.count = number;
     numberHTML.innerHTML = localStorage.count;
 }
 
@@ -18,12 +21,12 @@ function Lower(){
     if (number < 0) {
         number = 0;
     }
-    localStorage.count = String(number);
+    localStorage.count = number;
     numberHTML.innerHTML = localStorage.count;
 }
 
 function Reset(){
     number = 0;
-    localStorage.count = String(number);
+    localStorage.count = number;
     numberHTML.innerHTML = localStorage.count;
 }
